591314900bbe19fe5e8760cd9073717d8b40aa1f
Esteban Pastorino <ejpastorino@gmail.com>
2012-06-18 13:03:41 -0300

    move data to public folder

--- app.rb
+++ app.rb
@@ -1,10 +1,12 @@
-
 Cuba.settings[:template_engine] = "haml"
 Cuba.plugin Cuba::Render
 
 Cuba.use Rack::Static,
   root: "public",
-  urls: ["/javascripts", "/images", "/favicon.ico"]
+  urls: ["/javascripts", "/images", "/favicon.ico", "/data"]
+
+Cuba.use Rack::Deflater
+
 
 Cuba.define do
   on get do
@@ -16,14 +18,6 @@ Cuba.define do
       res.headers["Cache-Control"] = "public, max-age=29030400" if req.query_string =~ /[0-9]{10}/
       res.headers["Content-Type"] = "text/css; charset=utf-8"
       res.write render("views/stylesheets/#{file}.scss")
-    end
-
-    on "data/:year", extension("json") do |year, file|
-
-      res.headers["Cache-Control"] = "public, max-age=29030400" if req.query_string =~ /[0-9]{10}/
-      res.headers["Content-Type"] = "application/json; charset=utf-8"
-
-      res.write File.read("views/data/#{year}/#{file}.json")
     end
 
     on true do
